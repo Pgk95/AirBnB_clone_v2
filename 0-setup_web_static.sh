571a686818ea2d5acb1bb6a5a79919a0ca02ae00
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-07-08 09:53:06 +0000

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Install Nginx if not already installed
+
+if ! command -v nginx &> /dev/null; then
+        apt-get update
+        apt-get install -y nginx
+fi
+
+# Creates necessary directories
+mkdir -p /data/we_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file
+echo "Hello, PGK!" > /data/web_static/releases/test/index.html
+
+# Recreate the symbolic link
+rm -rf /data/web_static/current
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Set ownership of /data/ to ubuntu user adn group recursively
+chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+CONFIG_FILE="/etc/nginx/sites-available/default"
+STATIC_ALIAS="location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
+
+# Add or update alias in nginx configuration
+if grep -q "location /hbnb_static/" "$CONFIG_FILE"; then
+        sed -i "/location \/hbnb_static\//c\\$STATIC_ALIAS" "$CONFIG_FILE"
+else
+        sed -i "s#^}#$STATIC_ALIAS\n\n\t}#" "$CONFIG_FILE"
+fi
+
+# Restart Nginx
+service nginx restart
