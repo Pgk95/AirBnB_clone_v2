4b662979b78740c273c362cab92bb26f63702c7d
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-07-10 10:41:04 +0300

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""Fab file for compressing to an archive"""
+"""
+a Fabric script that generates a .tgz archive
+from the contents of the web_static folder of
+AirBnB Clone repo, using the function do_pack
+"""
+from fabric.api import local
+from time import strftime
 
-import os.path
-from fabric.api import task, local
-from datetime import datetime
 
-@task
 def do_pack():
-    """Creates the folder (versions) if it doesn't exist"""
-    local("mkdir -p versions")
-
-    """Creates the archive filename using current timestamp"""
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_filename = "web_static_{}.tgz".format(now)
-
-    """Creates the .tgz archive using tar command"""
-    local("tar -czvf versions/{} web_static".format(archive_filename))
-
-    """Return the archive path if the archive is generated (successfullt)"""
-    archive_path = "versions/{}".format(archive_filename)
-    if not os.path.isfile(archive_path):
+    """generaate a .tgz archive"""
+    timenow = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        archname = "versions/web_static_{}.tgz".format(timenow)
+        local("tar -cvzf {} web_static/".format(archname))
+        return archname
+    except:
         return None
-    else:
-        return archive_path
