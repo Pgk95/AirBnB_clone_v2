6b50affe4b87dd0c5949d8067298dd14aa619e15
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-07-22 19:29:04 +0000

    quick fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,12 +3,12 @@
 starts flask web application
 listens on 0.0.0.0, port 5000
 """
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_flashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     """returns the string (Hello, HBNB!)"""
     return "Hello, HBNB!"
