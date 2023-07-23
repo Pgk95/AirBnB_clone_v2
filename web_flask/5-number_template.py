3fd1730bccc2bd9c64151d3263c03a0b0c0f8fe9
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-07-23 20:40:18 +0000

    created template directory

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Starts a flas web appication"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """returns the string Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_text(text):
+    """Replace underscore (_) symbols with a space"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def show_python(text):
+    """Replace underscore (_) symbols with a space"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def show_number(n):
+    """displays n is a number if n is integer"""
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """displays HTML only if n is an integer"""
+    return render_template('number_template.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
