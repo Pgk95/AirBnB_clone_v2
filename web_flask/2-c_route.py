ac272a89282be0987573340ddaf3260ee7479c3f
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-07-23 20:16:13 +0000

    task 3

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Starts a flas web appication"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """returns the string Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_text(text):
+    """Replace underscore (_) symbols with a space"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
