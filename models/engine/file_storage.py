c6c3a50389bf616280733d24768ea0c846041147
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-07-24 09:49:46 +0000

    mysql, imroving the engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,8 @@ class FileStorage:
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             FileStorage.__objects.pop(key, None)
+    
+
+    def close(self):
+        """Call the method reload"""
+        self.reload()
